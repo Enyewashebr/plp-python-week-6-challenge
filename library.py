88e742e9f285da7ed9abe47621605c501a512d7a
Enyewashebr <Enyewashebir0@gmail.com>
2025-07-23 15:43:00 +0300

    Add initial implementation of library.py with math, random, and datetime examples

---
+++ library.py
@@ -0,0 +1,22 @@
+# Library
+# python libray is a collection of modules and packages that provide reusable code for various functionalities.
+# Example: Using the math library to perform mathematical operations
+
+import math
+print(math.sqrt(16))
+print(math.pi)
+print(math.factorial(5))
+
+
+# random library
+import random
+print(random.randint(1, 10))  # Random integer between 1 and 10
+print(random.choice(['apple', 'banana', 'cherry']))  # Random choice from a
+
+
+
+# datetime library
+import datetime
+tomorrow = datetime.date.today() #+ datetime.timedelta(days=1)
+print(tomorrow)
+print (tomorrow.day)  # Current date and time
