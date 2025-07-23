fa911aca5ca914411f5ee04dc2a5707eb9b5bf78
Enyewashebr <Enyewashebir0@gmail.com>
2025-07-23 17:24:38 +0300

    Add numpy.py with array operations and print statements

---
+++ numPy.py
@@ -0,0 +1,14 @@
+import numpy as np
+my_array = np.arange(10,51)
+array2=np.array([11,12,13,14,15])
+max_value = np.max(my_array)
+min_value=np.min(my_array)
+triple_value= my_array * 3
+
+
+print("max value:", max_value)
+print("min value:", min_value)
+print("triple value:", triple_value)
+print("Array 1:", my_array)
+print("Array 2:", array2)
+
